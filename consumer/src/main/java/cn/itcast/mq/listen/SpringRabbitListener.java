803f69b90f28ac09fa9769f4e38863e4f9173994
Liumingyao666 <1806859182@qq.com>
2023-08-02 14:37:20 +0800

    workQueue

--- consumer/src/main/java/cn/itcast/mq/listen/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/listen/SpringRabbitListener.java
@@ -3,11 +3,27 @@ package cn.itcast.mq.listen;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Component
 public class SpringRabbitListener {
 
+//    @RabbitListener(queues = "simple.queue")
+//    public void listenSimpleQueueMessage(String message){
+//        System.out.println("spring 消费者接收到消息：【" + message + "】");
+//    }
+
     @RabbitListener(queues = "simple.queue")
-    public void listenSimpleQueueMessage(String message){
-        System.out.println("spring 消费者接收到消息：【" + message + "】");
+    public void listenWorkQueue1Message(String message) throws InterruptedException {
+        System.out.println("spring 消费者1接收到消息：【" + message + "】" + LocalTime.now());
+        Thread.sleep(20);
     }
+
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenWorkQueue2Message(String message) throws InterruptedException {
+        System.out.println("spring 消费者2接收到消息：【" + message + "】" + LocalTime.now());
+        Thread.sleep(200);
+    }
+
 }
--- publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -20,4 +20,15 @@ public class SpringAmqpTest {
         String message = "hello, spring amqp!";
         rabbitTemplate.convertAndSend(queueName, message);
     }
+
+    @Test
+    public void workQueueTest() throws InterruptedException {
+        String queueName = "simple.queue";
+        String message = "message__";
+
+        for (int i = 0; i <= 50 ; i++) {
+            rabbitTemplate.convertAndSend(queueName, message+i);
+            Thread.sleep(20);
+        }
+    }
 }
